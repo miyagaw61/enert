a16a29c35b31a575f9c97f86f1a56222cc03b099
miyagaw61 <miyagaw61@gmail.com>
2017-09-16 01:05:50 +0900

    [new]setup.py :

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enert',
+    version='0.0.1'
+    packages=find_packages(),
+    description='miyagaw61\'s python library',
+    author='Taisei Miyagawa <Twitter: @miyagaw61>',
+    author_email='miyagaw61@gmail.com'
+    url='https://github.com/miyagaw61/enert.git',
+    license=GPL,
+)
