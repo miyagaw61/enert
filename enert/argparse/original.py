a816ec5af52d54d397668276b8f646457e601516
miyagaw61 <miyagaw61@gmail.com>
2017-09-19 16:09:39 +0900

    [update]. :

--- enert/argparse/original.py
+++ enert/argparse/original.py
@@ -1,6 +1,6 @@
 from .toplevel import *
 
-def mkparser(usage):
+def mkparser(usage, lst):
     """
     parser = mkparser(usage)
     parser.add_argument('command', choices=lst)
@@ -9,4 +9,7 @@ def mkparser(usage):
     """
     parser = ArgumentParser(usage=usage, formatter_class=RawDescriptionHelpFormatter, add_help=False)
     parser.add_argument('-h', '--help', action='store_true')
+    if not lst == None:
+        parser.add_argument('command', choices=lst)
+    parser.add_argument('args', nargs='*')
     return parser
