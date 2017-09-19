4465df3778c04a6303eede89883100666482756d
miyagaw61 <miyagaw61@gmail.com>
2017-09-19 16:14:59 +0900

    [update]. :

--- enert/argparse/original.py
+++ enert/argparse/original.py
@@ -2,10 +2,15 @@ from .toplevel import *
 
 def mkparser(usage, lst=None):
     """
-    parser = mkparser(usage)
-    parser.add_argument('command', choices=lst)
-    parser.add_argument('args', nargs='*')
-    args = parser.parse_args(argv[1:)
+    usage = '''\
+    Usage: git2nd [status] [add] [commit]
+    '''
+    lst = ['status', 'add', 'commit']
+    parser = mkparser(usage, lst)
+    args = parser.parse_args(argv[1:])
+    ->
+    args.command == status or add or commit
+    args.args == ['file01', 'file02']
     """
     parser = ArgumentParser(usage=usage, formatter_class=RawDescriptionHelpFormatter, add_help=False)
     parser.add_argument('-h', '--help', action='store_true')
