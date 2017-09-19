c9b4ba53e1b539bccfd07c5b1cebe5e02916cd94
miyagaw61 <miyagaw61@gmail.com>
2017-09-19 18:43:37 +0900

    [update]. : infのprinをconcatしないようにした

--- enert/enert.py
+++ enert/enert.py
@@ -372,18 +372,21 @@ def b():
     import pdb
     pdb.set_trace()
 
-def inf(strings, color="green"):
+def inf(data, color="green"):
     term_y, term_x = get_term_size()
     if color == "green":
         print(green("\n" + "="*term_x, "bold"))
-        print("[" + red("+", "bold") + "]" + strings)
+        sys.stdout.write("[" + red("+", "bold") + "]")
+        print(data)
         print(green("="*term_x + "\n", "bold"))
     if color == "red":
         print(red("\n" + "="*term_x, "bold"))
-        print("[" + red("+", "bold") + "]" + strings)
+        sys.stdout.write("[" + red("+", "bold") + "]")
+        print(data)
         print(red("="*term_x + "\n", "bold"))
     if color == "blue":
         print(blue("\n" + "="*term_x, "bold"))
-        print("[" + red("+", "bold") + "]" + strings)
+        sys.stdout.write("[" + red("+", "bold") + "]")
+        print(data)
         print(blue("="*term_x + "\n", "bold"))
 
