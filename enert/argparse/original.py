70cf667ef4c37ff646448282f4aadc5ce1c69641
miyagaw61 <miyagaw61@gmail.com>
2017-09-19 14:08:58 +0900

    [bugfix] :

--- enert/argparse/original.py
+++ enert/argparse/original.py
@@ -3,6 +3,7 @@ from .toplevel import *
 def mkparser(usage, lst):
     parser = ArgumentParser(usage=usage, formatter_class=RawDescriptionHelpFormatter, add_help=False)
     parser.add_argument('-h', '--help', action='store_true')
-    parser.add_argument('others', nargs='*')
-    parser.add_argument('command', choices=lst)
+    parser.add_argument('args', nargs='*')
+    if not lst == None:
+        parser.add_argument('command', choices=lst)
     return parser
